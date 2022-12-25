4170a7bd10730ccf7dbcc3b4199ec039808bfc9b
joseph.steeb <joseph.steeb@wsu.edu>
2022-12-25 01:29:13 -0800

    fixed gtk not found during build

---
+++ build/CMakeFiles/test_gtk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_gtk.dir/main.cpp.o"
+  "CMakeFiles/test_gtk.dir/main.cpp.o.d"
+  "test_gtk"
+  "test_gtk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_gtk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
